c0b9296edded9acad3546690ea7579a6b0df8678
Bumin Kim <kimbm@outlook.jp>
2019-02-12 01:17:37 +0900

    file

---
+++ scikit-learn/coo_matrix.py
@@ -0,0 +1,10 @@
+#%%
+from scipy import sparse
+import numpy
+
+data = numpy.ones(4)
+row_indices = numpy.arange(4)
+col_indices = numpy.arange(4)
+
+eye_coo = sparse.coo_matrix((data, (row_indices, col_indices)))
+print("COO representation:\n{}".format(eye_coo))
--- scikit-learn/learning_intro.py
+++ scikit-learn/learning_intro.py
@@ -1,4 +1,4 @@
-# %%
+#%%
 # numpy
 from scipy import sparse
 import numpy
@@ -7,9 +7,17 @@ x = numpy.array([[1, 2, 3], [4, 5, 6]])
 print("x: \n{}".format(x))
 
 
-# %%
+#%%
 # SciPy
 
 # 対角成分が１でそれ以上が0の、２次元NumPy配列を作る
+from scipy import sparse
+import numpy
 eye = numpy.eye(4)
 print("NumPy array:\n{}".format(eye))
+
+# Numpy配列をSciPyの形式のsparse matrixに変換して出力します。
+sparse_matrix = sparse.csr_matrix(eye)
+print("\nSciPy space SCR matrix:\n{}".format(sparse_matrix))
+
+#%%
