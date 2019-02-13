118c0e102fe20faaf43527fd1428b27eb9f498f4
Bumin Kim <kimbm@outlook.jp>
2019-02-14 07:08:16 +0900

    jupyter

--- scikit-learn/coo_matrix.py
+++ scikit-learn/coo_matrix.py
@@ -8,3 +8,6 @@ col_indices = numpy.arange(4)
 
 eye_coo = sparse.coo_matrix((data, (row_indices, col_indices)))
 print("COO representation:\n{}".format(eye_coo))
+
+#%%
+from Ipython import display
--- scikit-learn/learning_intro.py
+++ scikit-learn/learning_intro.py
@@ -16,8 +16,26 @@ import numpy
 eye = numpy.eye(4)
 print("NumPy array:\n{}".format(eye))
 
-# Numpy配列をSciPyの形式のsparse matrixに変換して出力します。
+# Numpy配列をSciPyの形式の疎行列(そぎょうれつ)に変換して出力します。
 sparse_matrix = sparse.csr_matrix(eye)
 print("\nSciPy space SCR matrix:\n{}".format(sparse_matrix))
 
 #%%
+from scipy import sparse
+import numpy
+
+data = numpy.ones(4)
+row_indices = numpy.arange(4)
+col_indices = numpy.arange(4)
+
+eye_coo = sparse.coo_matrix((data, (row_indices, col_indices)))
+print("COO representation:\n{}".format(eye_coo))
+
+#%%
+#%matplotlib inline
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.linspace(-10, 50, 500)
+y = numpy.sin(x)
+plt.plot(x, y, marker="x")
